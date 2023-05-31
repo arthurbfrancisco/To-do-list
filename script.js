013f0bedc838c216ac61cba9172f45684e928b67
Arthur Franciso <arthur.b.francisco@gmail.com>
2023-05-31 19:41:07 +0700

    update

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Lets get some some work done!");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
